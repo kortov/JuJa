aebd73d37a368b0e7d5f371ea7756b90d08914a1
Eugene Kortov <kortovea@gmail.com>
2018-02-13 23:52:27 +0400

    Task1 is done

---
+++ src/com/juja/kortov/multithreading/Task1/Solution.java
@@ -0,0 +1,19 @@
+package com.juja.kortov.multithreading.Task1;
+
+/* My first thread
+Создать public static class TestThread - нить с помощью интерфейса Runnable.
+TestThread должен выводить в консоль "My first thread".
+*/
+public class Solution {
+    public static void main(String[] args) {
+        TestThread task = new TestThread();
+        new Thread(task).start();
+    }
+}
+
+class TestThread extends Thread {
+    @Override
+    public void run() {
+        System.out.print("My first thread");
+    }
+}
---
+++ src/com/juja/kortov/multithreading/Task1/SolutionTest.java
@@ -0,0 +1,38 @@
+package com.juja.kortov.multithreading.Task1;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private PrintStream originalOut;
+    private PrintStream originalErr;
+
+    @Before
+    public void setUpStreamsAndDbManager() {
+        originalOut = System.out;
+        originalErr = System.err;
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    public void testConsole() {
+        String expected = "My first thread";
+        Solution.main(new String[]{});
+        assertEquals(expected, outContent.toString());
+    }
+}
